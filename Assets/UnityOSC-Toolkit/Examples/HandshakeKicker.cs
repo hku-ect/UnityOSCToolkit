35dc9f82b79dc94122870eadff7f7ef71cad5311
aaronvark <aoostdijk@gmail.com>
2017-01-20 12:13:11 +0100

    Fixed main-thread error when loading through handshaker. Added kicker example.

---
+++ Assets/UnityOSC-Toolkit/Examples/HandshakeKicker.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HKUECT;
+
+[RequireComponent(typeof(GearVRHandshaker))]
+public class HandshakeKicker : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        GetComponent<GearVRHandshaker>().Handshake();
+	}
+}
